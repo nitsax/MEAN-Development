6ac7f6a04c7a72f73a18d05aed104c1ff7a60467
nitsax <nitins.sax@gmail.com>
2015-09-17 01:19:52 +0530

    Adding pasport authentication

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -1,6 +1,56 @@
 var app=angular.module('flapperNews', ['ui.router'])
 
-app.factory('postService', ['$http', function($http) {
+app.factory('auth', ['$http', '$window', function($http, $window) {
+	var auth = {};
+
+	auth.saveToken = function(token) {
+		$window.localStorage['flapper-news-token'] = token;
+	};
+
+	auth.getToken = function() {
+		return $window.localStorage['flapper-news-token'];
+	};
+
+	auth.isLoggedIn = function() {
+		var token = auth.getToken();
+
+		if(token) {
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+			return payload.exp > Date.now() / 1000;
+		} else {
+			return false;
+		}
+	};
+
+	auth.currentUser = function() {
+		if(auth.isLoggedIn()) {
+			var token = auth.getToken();
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+			return payload.username;
+		}
+	};
+
+	auth.register = function(user) {
+		return $http.post('/register', user).success(function(data) {
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logIn = function(user) {
+		return $http.post('/login', user).success(function(data) {
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logOut = function() {
+		$window.localStorage.removeItem('flapper-news-token');
+	};
+
+	return auth;
+}])
+
+app.factory('postService', ['$http', 'auth', function($http, auth) {
 	var service = {
 		posts: []
 	};
@@ -12,14 +62,17 @@ app.factory('postService', ['$http', function($http) {
 	};
 
 	service.create = function(post) {
-		return $http.post('/posts', post).success(function(data) {
+		return $http.post('/posts', post, {
+			headers: {Authorization: 'Bearer ' + auth.getToken()}
+		}).success(function(data) {
 			service.posts.push(data);
 		});
 	};
 
 	service.upvote = function(post) {
-		return $http.put('/posts/' + post._id + '/upvote')
-			.success(function(data) {
+		return $http.put('/posts/' + post._id + '/upvote', null, {
+			headers: {Authorization: 'Bearer ' + auth.getToken()}
+		}).success(function(data) {
 				post.upvotes += 1;
 			});
 	};
@@ -32,14 +85,17 @@ app.factory('postService', ['$http', function($http) {
 	};
 
 	service.addComment = function(id, comment) { 
-		return $http.post('/posts/' + id + '/comments', comment);
+		return $http.post('/posts/' + id + '/comments', comment, {
+			headers: {Authorization: 'Bearer ' + auth.getToken()}
+		});
 	};
 
 	service.upvoteComment = function(post, comment) {
-		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
-			.success(function(data) {
+		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null, {
+			headers: {Authorization: 'Bearer ' + auth.getToken()}
+		}).success(function(data) {
 				comment.upvotes += 1;
-			});
+		});
 	};
 
 	return service;
@@ -48,8 +104,10 @@ app.factory('postService', ['$http', function($http) {
 app.controller('MainCtrl', [
 	'$scope',
 	'postService',
-	function($scope, postService) {
+	'auth',
+	function($scope, postService, auth) {
 		$scope.posts = postService.posts;
+		$scope.isLoggedIn = auth.isLoggedIn;
 		$scope.addPost = function() {
 			if(!$scope.title || $scope.title == '') {	return;	}
 			postService.create({
@@ -69,8 +127,10 @@ app.controller('PostsCtrl', [
 '$scope',
 'postService',
 'post',
-function($scope, postService, post) {
+'auth',
+function($scope, postService, post, auth) {
 	$scope.post = post;
+	$scope.isLoggedIn = auth.isLoggedIn;
 	$scope.addComment = function() {
 		if($scope.body === '') { return; }
 		postService.addComment(post._id, {
@@ -112,7 +172,60 @@ function($stateProvider, $urlRouterProvider) {
 	  		return postService.get($stateParams.id);
 	  	}]
 	  }
+	})
+	.state('register', {
+		url: '/register',
+		templateUrl: '/register.html',
+		controller: 'AuthCtrl',
+		onEnter: ['$state', 'auth', function($state, auth) {
+			if(auth.isLoggedIn()) {
+				$state.go('home');
+			}
+		}]
+	})
+	.state('login', {
+		url: '/login',
+		templateUrl: '/login.html',
+		controller: 'AuthCtrl',
+		onEnter: ['$state', 'auth', function($state, auth) {
+			if(auth.isLoggedIn()) {
+				$state.go('home');
+			}
+		}]
 	});
 		
 	$urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('AuthCtrl', [
+	'$scope',
+	'$state',
+	'auth',
+	function($scope, $state, auth) {
+		$scope.user = {};
+
+		$scope.register = function() {
+			auth.register($scope.user).error(function(error) {
+				$scope.error = error;
+			}).then(function() {
+				$state.go('home');
+			});
+		};
+
+		$scope.logIn = function() {
+			auth.logIn($scope.user).error(function(error) {
+				$scope.error = error;
+			}).then(function() {
+				$state.go('home');
+			});
+		};
+}])
+
+app.controller('NavCtrl', [
+	'$scope',
+	'auth',
+	function($scope, auth) { 
+		$scope.isLoggedIn = auth.isLoggedIn;
+		$scope.currentUser = auth.currentUser;
+		$scope.logout = auth.logOut;
 }]);
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var jwt = require('express-jwt');
 var router = express.Router();
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+var auth = jwt({ secret: 'SECRET', userProperty: 'payload'});
 
 router.param('post', function(req, res, next, id) {
 	var query = Post.findById(id);
@@ -41,9 +45,10 @@ router.get('/posts', function(req, res, next) {
 	});
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
 	var post = new Post(req.body);
-	
+	post.author = req.payload.username;
+
 	post.save(function(err, post) {
 		if(err) { return next(err);}
 
@@ -59,7 +64,7 @@ router.get('/posts/:post', function(req, res) {
 	});
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
 	req.post.upvote(function(err, post) {
 		if(err) { return next(err); }
 
@@ -67,10 +72,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 	});
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
 	var comment = new Comment(req.body);
 	comment.post = req.post;
-
+	comment.author = req.payload.username;
 	comment.save(function(err, comment) {
 		if(err) { return next(err); }
 
@@ -83,12 +88,44 @@ router.post('/posts/:post/comments', function(req, res, next) {
 	});
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
 	req.comment.upvote(function(err, comment) {
 		if(err) { return next(err); }
 
 		res.json(comment);
 	});
+});
+
+router.post('/register', function(req, res, next) {
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message: 'Please fill out all fields'});
+	}
+
+	var user = new User();
+	user.username = req.body.username;
+	user.setPassword(req.body.password);
+
+	user.save(function(err) {
+		if(err) { return next(err); }
+
+		return res.json({token: user.generateJWT()})
+	});
+});
+
+router.post('/login', function(req, res, next) {
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message: 'Please fill out all the fields'});
+	}
+
+	passport.authenticate('local', function(err, user, info) {
+		if(err) { return next(err); }
+
+		if(user) {
+			return res.json({token: user.generateJWT()});
+		} else {
+			return res.status(401).json(info);
+		}
+	})(req, res. next);
 });
 
 module.exports = router;
